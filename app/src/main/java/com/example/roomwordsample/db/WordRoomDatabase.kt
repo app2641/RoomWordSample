77fdec0f77555fc05b17bae1f4904501e3310f0f
app2641 <app2641@gmail.com>
2021-03-26 03:02:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/db/WordRoomDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.roomwordsample.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomwordsample.db.dao.WordDao
+import com.example.roomwordsample.db.entities.Word
+
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
