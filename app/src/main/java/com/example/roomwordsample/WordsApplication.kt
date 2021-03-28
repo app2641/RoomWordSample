9a604880add04047ea81948e60e99378d1a5fc70
app2641 <app2641@gmail.com>
2021-03-29 00:02:32 +0900

    Populate the database

--- app/src/main/java/com/example/roomwordsample/WordsApplication.kt
+++ app/src/main/java/com/example/roomwordsample/WordsApplication.kt
@@ -3,8 +3,11 @@ package com.example.roomwordsample
 import android.app.Application
 import com.example.roomwordsample.db.WordRoomDatabase
 import com.example.roomwordsample.db.repositories.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, this.applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
