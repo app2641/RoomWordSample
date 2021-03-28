e812fe96cc7fd5c9eab1d1cd198f2414dd25acc8
app2641 <app2641@gmail.com>
2021-03-28 23:05:24 +0900

    Setup RecyclerView

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.roomwordsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomwordsample.adapter.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
